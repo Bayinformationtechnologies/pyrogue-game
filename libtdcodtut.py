237d48043787015d581453f1eb3f4aa92809b2f6
nicfitzgerald <nicholas.a.fitzgerald@gmail.com>
2017-05-20 15:54:46 -0500

    Added generialized object; changed loops

--- libtdcodtut.py
+++ libtdcodtut.py
@@ -5,9 +5,30 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20 # Limits the FPS of the game
 
-def handle_keys ():
-    global playerx, playery
+class Object:
+    # This is a generic object: the player, monster, an item, stairs...
+    # It's always represented by char on screen
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
 
+    def move(self, dx, dy):
+        # Move by the given amount
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        # Set color and then draw the character that represents this object
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        # Erase the character that represents this object
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+def handle_keys ():
     key = libtcod.console_check_for_keypress()
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -19,16 +40,16 @@ def handle_keys ():
 
     # Movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        player.move(0, -1)
  
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        player.move(0,1)
  
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
+        player.move(-1,0)
  
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        player.move(1,0)
 
 #############################################
 # Initialization & Main Loop
@@ -36,22 +57,32 @@ def handle_keys ():
 
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT,'python/libtcod tutorial', False)
-con = libtcod.console_new(SCREEN_WIDTH,SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS)
+con = libtcod.console_new(SCREEN_WIDTH,SCREEN_HEIGHT)
 
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+# Object representing player
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+
+# Create a NPC
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+
+# List of objects with players
+objects = [npc, player]
 
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(con, libtcod.white)
-    libtcod.console_put_char(con, playerx, playery, '@',libtcod.BKGND_NONE)
+    
+    # Draw objects in list
+    for object in objects:
+        object.draw()
 
+    # Blit the contents of "con" to the root console and present it
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
-
-
     libtcod.console_flush()
 
-    libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
+    # Erase all objects at their old locations, before they move
+    for object in objects:
+        object.clear()
+
     # Handle keys and exit game if needed
     exit = handle_keys()
     if exit:
