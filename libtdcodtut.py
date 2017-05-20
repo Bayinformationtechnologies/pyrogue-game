8b35cb7e47538c2d3b1fc36775eeb7a3ed502ef3
nicfitzgerald <nicholas.a.fitzgerald@gmail.com>
2017-05-20 15:38:53 -0500

    Changed to off-screen console

--- libtdcodtut.py
+++ libtdcodtut.py
@@ -36,18 +36,22 @@ def handle_keys ():
 
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT,'python/libtcod tutorial', False)
+con = libtcod.console_new(SCREEN_WIDTH,SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS)
 
 playerx = SCREEN_WIDTH/2
 playery = SCREEN_HEIGHT/2
 
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, playerx, playery, '@',libtcod.BKGND_NONE)
+    libtcod.console_set_default_foreground(con, libtcod.white)
+    libtcod.console_put_char(con, playerx, playery, '@',libtcod.BKGND_NONE)
+
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
 
     libtcod.console_flush()
 
-    libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+    libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
     # Handle keys and exit game if needed
     exit = handle_keys()
     if exit:
